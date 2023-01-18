3653d4d6a8cbfb95e5bcd688f466fc9f620807ec
LeonidHome <defenser2205@gmail.com>
2023-01-18 23:12:31 +0300

    Task 8-10

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,9 @@ public class Main {
         task5();
         task6();
         task7();
+        task8();
+        task9();
+        task10();
     }
 
     public static void task1() {
@@ -55,6 +58,35 @@ public class Main {
         System.out.println("\nЗадача 7");
         for (int i = 1; i <= 512; i *= 2) {
             System.out.print(i + " ");
+        }
+    }
+
+    public static void task8() {
+        System.out.println("\nЗадача 8");
+        int money = 29000;
+        int capital = 0;
+        for (int i = 1; i <= 12; i++) {
+            capital = capital + money;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + capital);
+        }
+    }
+
+    public static void task9() {
+        System.out.println("Задача 9");
+        int money = 29000;
+        int capital = 0;
+        for (int i = 1; i <= 12; i++) {
+            capital = capital + money;
+            capital = capital + capital / 100;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + capital);
+        }
+    }
+
+    public static void task10() {
+        System.out.println("Задача 10");
+        int a = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(a + "*" + i + "=" + a*i);
         }
     }
 }
